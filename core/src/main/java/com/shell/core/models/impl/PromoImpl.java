93a0659ef39c84947db957fc8b1ca9eadcec419d
chandrikanarayan <chandrikan@digiquadsolutions.com>
2025-05-21 17:45:51 +0530

    DQSHBE-15 Completed Promobanner Compoment

--- core/src/main/java/com/shell/core/models/impl/PromoImpl.java
+++ core/src/main/java/com/shell/core/models/impl/PromoImpl.java
@@ -8,7 +8,6 @@ import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.List;
 /**
  * @author N.Chandrika
